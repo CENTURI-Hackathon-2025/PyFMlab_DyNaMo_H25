3c99596da8b9f365c7e12431ec28201f2c094010
Wenjie SUN <sunwjie@gmail.com>
2025-06-21 17:08:44 +0200

    Remove concurrent in Loadfiles

--- PyFMGUI_DyNaMo/src/pyfmgui/loadfiles.py
+++ PyFMGUI_DyNaMo/src/pyfmgui/loadfiles.py
@@ -1,8 +1,6 @@
 # Import logging and get global logger
 import logging
 logger = logging.getLogger()
-# Import for multiprocessing
-import concurrent.futures
 # Get loadfile function from PyFMReader
 from pyfmreader import loadfile
 # Get constants
@@ -23,13 +21,20 @@ def loadfiles(session, filelist, progress_callback, range_callback, step_callbac
     files_to_load = [path for path in filelist if path not in session.loaded_files_paths]
     loaded_files = []
     count = 0
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        # loaded_files = executor.map(load_single_file, files_to_load)
-        futures = [executor.submit(load_single_file, filepath) for filepath in files_to_load]
-        for future in concurrent.futures.as_completed(futures):
-            loaded_files.append(future.result())
-            count+=1
-            progress_callback.emit(count)
+    # with concurrent.futures.ProcessPoolExecutor() as executor:
+    #     # loaded_files = executor.map(load_single_file, files_to_load)
+    #     futures = [executor.submit(load_single_file, filepath) for filepath in files_to_load]
+    #     for future in concurrent.futures.as_completed(futures):
+    #         loaded_files.append(future.result())
+    #         count+=1
+    #         progress_callback.emit(count)
+
+    # No parallel processing
+    for filepath in files_to_load:
+        loaded_files.append(load_single_file(filepath))
+        count += 1
+        progress_callback.emit(count)
+
     # loaded_files = list(loaded_files)
     # Loop and save files in the session
     for file_id, file in loaded_files:
